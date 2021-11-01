e2a027dcef75a635b59259b14fc39ab115e14c3a
vanessa <vanessa@localhost>
2021-11-01 03:12:37 +0100

    voyage-voyage quand je suis contente je "commit" mais je dois aller dormir là

--- Voyage Voyage/index.php
+++ Voyage Voyage/index.php
@@ -1,8 +1,16 @@
+<? php
 // variable1 = budget
 //variable2 = tps_voyage_max
 // echo 'le voyage idéal'
-//$prix_base = '10€'
-//$tps_base = '3h'
+
 //$diminution = '2€' // 10€
 //$augmentation = '30min' // 3h
+
+// Il faudrait déjà créer le trajet initial pour avoir une base
+$tps_base = 3;
+$prix_base = 10;
+// faire une variable pour indiquer les prix et temps client $tps_client $prix_client
+
+// if ($prix_base > $prix_client)
+// else if ($tps_base > $tps_client)
 
